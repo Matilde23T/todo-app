df0929ba78f4d961ecf66b3ea2e2201538f461ff
Matilde23T <mati.tiengo02@gmail.com>
2025-09-16 22:31:15 +0200

    cambio link

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,7 @@
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = "https://todo-app-ca2p.onrender.com/api";
 console.log("API_BASE_URL:", API_BASE_URL);
+
 
 class ApiService {
   async request(endpoint, options = {}) {

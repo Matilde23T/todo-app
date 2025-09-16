472fb43600843d39660eb055c923e53da0e6c3f0
Matilde23T <mati.tiengo02@gmail.com>
2025-09-16 20:22:08 +0200

    modifiche backend e api

--- backend-demo/server.js
+++ backend-demo/server.js
@@ -17,4 +17,4 @@ app.use("/api/login", authRoutes);
 app.use("/api/todos", todosRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`server in http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
